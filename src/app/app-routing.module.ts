5dff990fee80c4b9591221c2517c2662e3310d3b
giovana.arisono <giovana.iwamizu@grupoagtech.com>
2022-09-17 20:10:13 -0300

    menu to access the projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { QRCodeComponent } from './qrcode/qrcode.component';
 
 const routes: Routes = [
-  { path: "", component: QRCodeComponent },
+  { path: "", component: HomeComponent },
+  { path: "QRCode", component: QRCodeComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QRCodeComponent } from './qrcode/qrcode.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QRCodeComponent
+    QRCodeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
