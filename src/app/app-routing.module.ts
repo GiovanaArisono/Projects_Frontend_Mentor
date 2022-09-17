3abc9dfd643ab571de851852f6b9a7686df31d54
giovana.arisono <giovana.iwamizu@grupoagtech.com>
2022-09-17 19:43:54 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QRCodeComponent } from './qrcode/qrcode.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: QRCodeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
